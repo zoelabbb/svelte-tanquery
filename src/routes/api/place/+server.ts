cd409d80f80c4f23e1d8accd41777f6cfc237734
zoelabbb <zoelabbb@gmail.com>
2026-05-31 11:31:40 +0700

    Add .env.example

--- src/routes/api/place/+server.ts
+++ src/routes/api/place/+server.ts
@@ -1,11 +1,12 @@
 import type { RequestHandler } from '@sveltejs/kit';
+import { PUBLIC_API_URL } from '$env/static/public';
 
 export const GET: RequestHandler = async ({ url }) => {
 	try {
 		const page = url.searchParams.get('page') || '1';
 		const limit = url.searchParams.get('limit') || '10';
 		
-		const apiUrl = `http://localhost:6080/v1/api/place?page=${page}&limit=${limit}`;
+		const apiUrl = `${PUBLIC_API_URL}/place?page=${page}&limit=${limit}`;
 
 		const res = await fetch(apiUrl);
 		if (!res.ok) {
--- src/routes/api/place/[id]/+server.ts
+++ src/routes/api/place/[id]/+server.ts
@@ -1,9 +1,10 @@
 import type { RequestHandler } from '@sveltejs/kit';
+import { PUBLIC_API_URL } from '$env/static/public';
 
 export const GET: RequestHandler = async ({ params }) => {
 	try {
 		const { id } = params;
-		const apiUrl = `http://localhost:6080/v1/api/place/${id}`;
+		const apiUrl = `${PUBLIC_API_URL}/place/${id}`;
 
 		const res = await fetch(apiUrl);
 		if (!res.ok) {
